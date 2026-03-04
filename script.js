cbc5437b0149149e274d136106deb176fd8b3a06
taniashahida-dev <taniashahida993@gmail.com>
2026-03-04 15:31:04 +0600

    Add modal

--- script.js
+++ script.js
@@ -19,6 +19,43 @@ removeActive()
         displayLevelWord(data.data)
     })
 }
+
+const loadWordDetails = async(id)=>{
+    const url = `https://openapi.programming-hero.com/api/word/${id}`
+    const res = await fetch(url)
+    const details =await res.json()
+    displayWordDetails(details.data)
+
+
+
+}
+const displayWordDetails = (word)=>{
+    // console.log(word)
+    const modalContainer = document.getElementById('modal-container')
+    modalContainer.innerHTML=`
+  
+   
+        <h1 class="font-bold text-2xl">${word.word}(<i class="fa-solid fa-microphone-lines"></i> :${word.pronunciation})</h1>
+        <div>
+          <h5 class="font-bold">Meaning</h5>
+          <p>${word.meaning}</p>
+        </div>
+        <div>
+          <h5 class="font-bold">Example</h5>
+          <p>${word.sentence}</p>
+        </div>
+        <div>
+          <h5 class="font-bold">সমার্থক শব্দ গুলো</h5>
+          <button class="btn bg-blue-200">sync1</button>
+          <button class="btn bg-blue-200">sync1</button>
+          <button class="btn bg-blue-200">sync1</button>
+        </div>
+        <button class="btn btn-primary">Complete Learning</button>
+      
+    `
+    document.getElementById('word_modal').showModal()
+}
+
 const displayLevelWord=(words)=>{
 
     const wordContainer = document.getElementById('word-container')
@@ -47,7 +84,7 @@ const displayLevelWord=(words)=>{
   <p  class="font-semibold">${word.pronunciation?word.pronunciation:"pronunciation Pawa jayni"}</p>
   <p class="bangla-font font-medium text-2xl">${word.meaning?word.meaning:"Meaning Pawa Jayni"}</p>
   <div class="flex justify-between">
-  <button class="btn bg-gray-200 hover:bg-gray-400"><i class="fa-solid fa-circle-info"></i></button>
+  <button  onclick="loadWordDetails(${word.id})" class="btn bg-gray-200 hover:bg-gray-400"><i class="fa-solid fa-circle-info"></i></button>
   <button class="btn bg-gray-200 hover:bg-gray-400"><i class="fa-solid fa-volume-high"></i></button>
 </div>
 </div>
